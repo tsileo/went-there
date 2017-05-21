f4fcf65d82516aab0c920891bb6cabfa6cfe4948
Thomas Sileo <t@a4.io>
2017-05-21 14:01:19 +0200

    Working app

--- app/app.lua
+++ app/app.lua
@@ -1,17 +1,13 @@
-print('went-there')
-ts = require('timeseries').new('lol')
-rg = require('reversegeo')
+local timeseries = require('timeseries')
+local reversegeo = require('reversegeo')
 
-place = rg.reversegeo{lat = 30.2671, lng = -87.74306}
-print(place.data.city_name)
-print(place.lat)
-print(place.lng)
-ts:insert({omg = 2}, 5)
-data, _ = ts:range(-1, 0)
-print(data)
-for i = 1, #data do
-  print(data[i].time)
-  print(data[i].data.omg)
-end
-app.response:write('hello')
-print('ol')
+local ts = timeseries.new('log')
+
+local args = app.request:args()
+local point = {lat = tonumber(args:get('lat')), lng = tonumber(args:get('lng'))}
+
+local reversegeo_point = reversegeo.reversegeo(point)
+
+ts:insert{location = point, reverse_geo = reversegeo_point}
+
+app.response:set_status(204)
