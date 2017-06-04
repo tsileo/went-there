b1b624e86e240c16cc246273fb666c44af9eb916
Thomas Sileo <t@a4.io>
2017-06-04 16:42:48 +0200

    Added some docs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # went-there
 
-Lua application built to run on [ge0](https://github.com/tsileo/ge0) that I use to track my locations.
+Lua application built to run on [ge0](https://github.com/tsileo/ge0) that I use to track my locations using [Are You Tracking Me?](https://github.com/tsileo/are-you-tracking-me).
 
 Open-sourced to demo what `ge0` can do, no external contributions will be accepted, but feel free to fork the project.
--- app/app.lua
+++ app/app.lua
@@ -3,10 +3,15 @@ local reversegeo = require('reversegeo')
 
 local ts = timeseries.new('log')
 
+-- Are You Tracking Me? will send POST requests that looks like:
+-- {"lat": 1.0, "lng": 1.0, "device_id": <Android device ID>, "ts": <timestamp UTC in ms>}
 local payload = app.request:body():json()
 
+-- Get the reverse geolocation data
 local reversegeo_point = reversegeo.reversegeo(payload)
 
+-- Insert the event in the "log" timeseries
 ts:insert({location = payload, reverse_geo = reversegeo_point}, payload.ts)
 
+-- Returns a 201 Created
 app.response:set_status(201)
