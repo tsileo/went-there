4099156a0e60ed037de65dc9508eb75844931676
Thomas Sileo <t@a4.io>
2017-06-08 22:21:20 +0200

    Updated to work with batch data

--- app/app.lua
+++ app/app.lua
@@ -4,14 +4,18 @@ local reversegeo = require('reversegeo')
 local ts = timeseries.new('log')
 
 -- Are You Tracking Me? will send POST requests that looks like:
--- {"lat": 1.0, "lng": 1.0, "device_id": <Android device ID>, "ts": <timestamp UTC in ms>}
+-- {"device_id": <Android device ID>, "locations":[{"lat": 1.0, "lng": 1.0, , "ts": <timestamp UTC in ms>}]}
 local payload = app.request:body():json()
 
--- Get the reverse geolocation data
-local reversegeo_point = reversegeo.reversegeo(payload)
+for _, loc in ipairs(payload.locations) do
+  -- Get the reverse geolocation data
+  local reversegeo_point = reversegeo.reversegeo(loc)
 
--- Insert the event in the "log" timeseries
-ts:insert({location = payload, reverse_geo = reversegeo_point}, payload.ts)
+  loc.device_id = payload.device_id
+
+  -- Insert the event in the "log" timeseries
+  ts:insert({location = loc, reverse_geo = reversegeo_point}, loc.ts)
+end
 
 -- Returns a 201 Created
 app.response:set_status(201)
